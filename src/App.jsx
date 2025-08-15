18f13098441760edb5e7ce36303b30d8df99f762
Yousef Mohsen <ymohsen746@gmail.com>
2025-08-15 20:58:40 +0300

    replacing createbroweserrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./components/Home";
 import MealDetails from "./components/MealDetails";
 import Category from "./components/Category";
 function App() {
-  const router = createBrowserRouter([
+  const router = createHashRouter([
     {
       path: "/",
       element: <Layout />,
